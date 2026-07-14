a59e78bb24aab9850bbe3d1325783893a4343aa9
Bibah-19 <ceejbeat@gmail.com>
2026-07-14 14:39:15 +0100

    first deploy

---
+++ index.js
@@ -0,0 +1,51 @@
+   let ListOfNames = [
+    { 'name': 'john', 'lastName': 'ojasanya', 'joinedYear': '2026', 'level': 200, 'Gender': 'F', 'image': 'https://thumbs.dreamstime.com/b/portrait-young-african-american-business-woman-black-peop-isolated-white-background-people-33803836.jpg',  },
+    { 'name': 'ceej', 'lastName': 'hen', 'joinedYear': '2025', 'level': 300, 'Gender': 'M', 'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS06v8Z81RM4xhSEEjJ1ROXNVAnmpPFjwTiInJGy8BD4w&s=10'},
+    { 'name': 'jay', 'lastName': 'john', 'joinedYear': '2022', 'level': 400, 'Gender': 'F', 'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS78pDXzx0ojE93zEeBfoM-3bfY7tfWiF4YNN8LR5yvWg&s=10' },
+    { 'name': 'ceejjay', 'lastName': 'max', 'joinedYear': '2023', 'level': 100, 'Gender': 'M', 'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOM2Xwn0dHJpVXG3jI2cRJ2dkQYgsH4Nxc28DFzBoRqQ&s=10' }
+]
+
+    let index = 0
+
+    function display() {
+           image01.src = ListOfNames[index].image
+         nameDisplay.textContent = 'Name:' +  ListOfNames[index].name
+         lastname.textContent = 'Last Name:' + ListOfNames[index].lastName
+        lastyear.textContent = 'Year joined:' + ListOfNames[index].joinedYear
+        result.textContent = 'level:' + ListOfNames[index].level
+        Gendertype.textContent = 'Gender:' + ListOfNames[index].Gender
+    }
+    display()
+  
+  
+
+    function nextButton() {
+        
+
+        if (index === ListOfNames.length -1) {
+            index = 0
+            image01.src = ListOfNames[index]
+            
+        } else {
+            image01.src =` ${ListOfNames[++index]} `
+        
+        }
+
+        display()
+
+
+    }
+
+    function previousButton() {
+        if (index === 0) { 
+            index = ListOfNames.length -1
+            image01.src = images[index]
+            
+        } else {
+            image01.src = images[--index]       
+        }
+         display()
+       
+        
+    }
+
